60a271a3bba94a092cf81bbbf7eec37f319ed21a
EgorKopyl <91937091+EgorKopyl@users.noreply.github.com>
2022-02-22 17:14:53 +0200

    Update Animal.java

--- src/domain/Animal.java
+++ src/domain/Animal.java
@@ -2,27 +2,27 @@ package domain;
 
 public class Animal {
 
-    protected String name;
+    String name;
 
-    protected int weight;
+    int weight;
+
+    int age;
 
     public Animal() {
         name="generic animal";
-        weight=25;
+        weight=8;
+        age=3;
     }
 
-    public void eat() {
+    public void Eat() {
         System.out.println("Animal eating...");
     }
 
     public void speak() {
         System.out.println("Animal speaking...");
     }
-
-    @Override
+@Override
     public String toString() {
-        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg";
+        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg"+"\nAge:\t"+age+" years";
     }
-
- 
 }
