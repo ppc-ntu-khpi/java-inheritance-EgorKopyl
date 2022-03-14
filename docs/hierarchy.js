276de940d664b2fc036a8bdf6acdf086b7a49598
EgorKopyl <91937091+EgorKopyl@users.noreply.github.com>
2022-03-14 21:28:10 +0200

    Add files via upload

---
+++ docs/hierarchy.js
@@ -0,0 +1,9 @@
+var hierarchy =
+[
+    [ "domain.Animal", "classdomain_1_1_animal.html", [
+      [ "domain.Predator", "classdomain_1_1_predator.html", [
+        [ "domain.Fox", "classdomain_1_1_fox.html", null ]
+      ] ]
+    ] ],
+    [ "test.TestAnimal", "classtest_1_1_test_animal.html", null ]
+];
