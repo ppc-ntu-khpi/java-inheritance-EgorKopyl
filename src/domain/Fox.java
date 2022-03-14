63ab72859c0761d3fd7bc392b211b8e2d8d21b99
EgorKopyl <91937091+EgorKopyl@users.noreply.github.com>
2022-03-14 14:40:40 +0200

    Update Fox.java

--- src/domain/Fox.java
+++ src/domain/Fox.java
@@ -1,43 +1,116 @@
 package domain;
 
+
+ /**
+ * The class Fox extends predator
+ */ 
 public class Fox extends Predator {
 
     private String kind;
 
-    public Fox(String name ,int weight, int age, String kind) {
+
+/** 
+ *
+ * Fox
+ *
+ * @param name  the name
+ * @param weight  the weight
+ * @param age  the age
+ * @param kind  the kind
+ */
+    public Fox(String name ,int weight, int age, String kind) { 
+
         this.name=name;
         this.weight=weight;
         this.age=age;
         this.kind=kind;
     }
 
-    public Fox() {
+
+/** 
+ *
+ * Fox
+ *
+ */
+    public Fox() { 
+
         this("Mery",8,3,"just a fox");
     }
-    public Fox(String name) {
+
+/** 
+ *
+ * Fox
+ *
+ * @param name  the name
+ */
+    public Fox(String name) { 
+
         this(name,8,3,"just a fox");
     }
-    public void Fox() {
+
+/** 
+ *
+ * Fox
+ *
+ */
+    public void Fox() { 
+
     }
 
-    public void play() {
+
+/** 
+ *
+ * Play
+ *
+ */
+    public void play() { 
+
         System.out.println("The fox plays with fox cubs");
     }
     @Override
-    public void hunt() {
+
+/** 
+ *
+ * Hunt
+ *
+ */
+    public void hunt() { 
+
         System.out.println("Fox hunting for a mouse, rebbit and chicken");
     }
     @Override
-    public String toString() {
+
+/** 
+ *
+ * To string
+ *
+ * 
+ */
+    public String toString() { 
+
         return super.toString()+"\nKind:\t"+this.kind+"\n\nThis is Fox!";
     }
 
     @Override
-    public void speak() {
+
+/** 
+ *
+ * Speak
+ *
+ */
+    public void speak() { 
+
         System.out.println("Yap-yap-yap!Yap-yap-yap!");
     }
 
-    public void eat() {
+
+/** 
+ *
+ * Eat
+ *
+ */
+    public void eat() { 
+
         System.out.println("Fox eats meat and berries, and other");
     }
 }
